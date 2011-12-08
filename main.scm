ef2326f61da64db805df2a705501f1fe1d2bbaee
Jason Lee <lee.jason.park@gmail.com>
2011-12-08 16:03:08 -0600

    created an intro sequence, skippable by pressing space

--- main.scm
+++ main.scm
@@ -9,26 +9,37 @@
 (define-twig-object announcer Adult @(0 0 0) @(0 0 1)
   Color: Color.Red)
 (load "HUDs.scm")
-(load "level.scm")
+
 (titles.Say (String.Format "Gold: {0}  Lives: {1}" gold lives) 0.0 0.0)
 (define all-characters (list announcer))
 
 (define (random-point)
   (let ((p (MathUtil.NoiseVector 15)))
     (if (< (magnitude p) 8)
-	p
-
-	(random-point))))
+        p        
+        (random-point))))
 
 (define-twig-object gameCamera Camera)
+(within announcer
+        (load "intro.scm")
+        
+        (define-state-machine allgame
+          (pre (when (key-down? Keys.Space) (goto post)))
+          (post (enter 
+                 (dialog.SetState "done") (set-timeout 0))
+                (messages (TimeoutMessage (goto ready))))
+          (ready (enter 
+                  (load "level.scm")
+                  (within gameCamera
+                          (load "camera.scm")
+                          (load "towermanager.scm"))
+                  (load "enemies.scm")
+                  ))))
 
 
-(within gameCamera
-        (load "camera.scm")
 
-(load "towermanager.scm"))
 
-(load "enemies.scm")
+
 
 
 
